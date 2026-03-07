bfe621b29df941b0cf8b0e3d070d5c476c74287a
Jonnathan23 <jsaquicela26@gmail.com>
2026-03-07 18:30:48 -0500

    configuraciones para comunicaciones entre los sistemas packages y apps

--- apps/admin-desk/vite.config.ts
+++ apps/admin-desk/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@salc/core': fileURLToPath(new URL('../../packages/core/src', import.meta.url)),
+      '@salc/ui': fileURLToPath(new URL('../../packages/ui', import.meta.url)),
+      '@salc/ui/*': fileURLToPath(new URL('../../packages/ui/*', import.meta.url))
+    }
+  }
 })
