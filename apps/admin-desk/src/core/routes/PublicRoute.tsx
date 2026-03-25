8cb81f0f6a47463362e2c4fc7f66c14902f40a86
Jonnathan23 <jsaquicela26@gmail.com>
2026-03-24 23:36:39 -0500

    proteccion de la ruta en base a los permisos

--- apps/admin-desk/src/core/routes/PublicRoute.tsx
+++ apps/admin-desk/src/core/routes/PublicRoute.tsx
@@ -5,7 +5,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 export const PublicRoute = () => {
     const { isAuthenticated } = useAuthStore();
 
-    // Si ya está autenticado y quiere entrar al login, lo redirigimos a su panel
     if (isAuthenticated) {
         return <Navigate to="/" replace />;
     }
