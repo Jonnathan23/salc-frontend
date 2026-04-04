78796dc8855cedd0652ce9aca997fe5b34f1ff5c
Jonnathan23 <jsaquicela23@gmail.com>
2026-04-04 16:09:33 -0500

    implementacion de control de permisos en la vista

--- apps/admin-desk/src/features/indentity/presentation/pages/ViewProfile.page.tsx
+++ apps/admin-desk/src/features/indentity/presentation/pages/ViewProfile.page.tsx
@@ -4,14 +4,21 @@ import { useFindUserById } from "@/features/indentity/application/hooks";
 import { Loader2 } from "lucide-react";
 import { Button } from "@/core/components/buttons/button";
 
-import UpdateUser from "../components/register-update/UpdateUser";
-import { useState } from "react";
-import ProfileData from "../components/profiles/ProfileData";
+import UpdateUser from "@/features/indentity/presentation/components/register-update/UpdateUser";
+import { useMemo, useState } from "react";
+import ProfileData from "@/features/indentity/presentation/components/profiles/ProfileData";
+import { useAuthStore } from "@/features/indentity/application/store/auth.store";
+import { systemPermissions } from "@salc/core/enums/Permissions";
+
+
+
 
 export default function ViewProfilePage() {
     const params = useParams();
     const userId = params.userId!;
     const navigation = useNavigate();
+
+    const { userResponse } = useAuthStore();
 
     const [isEdit, setIsEdit] = useState(false);
     const { data: successResponse, isLoading, isError } = useFindUserById(userId);
@@ -19,10 +26,13 @@ export default function ViewProfilePage() {
 
     const handleSetEdit = () => setIsEdit(!isEdit);
 
+    const canUserResponseEdit = useMemo(() => {
+        if (!userResponse) return false;
+        return userResponse.permissions.includes(systemPermissions.SHARED_IDENTITY_WRITE);
+    }, [userResponse]);
 
-    if (isLoading) {
-        return <Loader2 className="w-4 h-4 animate-spin" />;
-    }
+
+    if (isLoading) return <Loader2 className="w-4 h-4 animate-spin" />;
 
     if (isError || !user) {
         return (
@@ -33,8 +43,7 @@ export default function ViewProfilePage() {
         )
     }
 
-    if (isEdit) return <UpdateUser userId={userId} user={user} handleSetEdit={handleSetEdit} />
+    if (isEdit && canUserResponseEdit) return <UpdateUser userId={userId} user={user} handleSetEdit={handleSetEdit} />
 
     return <ProfileData user={user} handleSetEdit={handleSetEdit} />
-
 }
